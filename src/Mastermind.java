ba9670e5487036cc5c85d0a96d1600540166656f
Javier <106848262+javiiicz@users.noreply.github.com>
2023-10-02 22:43:58 -0400

    Added codeword generator and game loop
    Main calls playPartOne which has the loop. Inside, generateCodeword is called and at the end anitherGame.

--- src/Mastermind.java
+++ src/Mastermind.java
@@ -1,8 +1,9 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
 
 public class Mastermind {
     public static void main(String[] args) {
+        playPartOne();
         
     } // end of main method
 
@@ -45,5 +46,56 @@ public class Mastermind {
         // This uses the method specified in the project instructions
 
     } // end of getMisses method
+
+
+    // Generates a 4 character codeword as a string (chars are nums 1-6)
+    public static String generateCodeword() {
+        String result = "";
+        for ( int i = 0; i<4; i++) {
+            int num = (int)Math.ceil(Math.random()*6); // get random int from 1 to 6
+            result += num; // append to result
+        }
+        return result;
+    } // end of generateCodeword method
+
+
+    // Executes the code to play Part 1 of the project
+    public static void playPartOne(){
+        Scanner keyboard = new Scanner(System.in);
+        boolean moreGames = true;
+        
+        while ( moreGames ) {
+            System.out.println("---- Lets play Mastermind ---");
+            String codeword = generateCodeword(); // creates codeword
+            
+            System.out.println(codeword);
+
+
+            if (!anotherGame(keyboard)) { // asks if user wants to play another game
+                moreGames = false;
+            }
+        } 
+    } // end of playPartOne method
+
+
+    // Checks validity of responses and returns true if 'y', false if otherwise
+    public static boolean anotherGame( Scanner in){
+        System.out.print("Another game (y/n)? "); // Replay
+        String action = "";
+        boolean responseValid = false;
+
+        while (!responseValid) {
+            action = in.next();
+            if (action.charAt(0) == 'y' || action.charAt(0) == 'n') {
+                responseValid = true;
+            }
+            else{
+                System.out.println("Invalid character, try again");
+            }
+        }
+
+        return action.charAt(0) == 'y';
+    } // end of anotherGame method
+    
 
 } // end of Mastermind class
